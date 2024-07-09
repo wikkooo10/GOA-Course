ae644f0073a7eafca31716ab4d623a7f10dd98a5
wikkooo10 <wikomxeciii@gmail.com>
2024-07-09 16:42:50 +0400

    first commit

---
+++ Day 01/Homework/lesson.py
@@ -0,0 +1,9 @@
+name = "irakli"
+last_name = "wiklauri"
+age = 14
+hobby = "judo"
+country = "georgia"
+
+print(name, last_name, age, hobby, country)
+print(name+last_name+str(age)+hobby+country)
+print("i am" +" "+ name+" "+last_name +" "+"i am"+" " + str(age) +"years old"+" " +"my hobby is"+" " +hobby+" "+"i live in"+" "+country)
